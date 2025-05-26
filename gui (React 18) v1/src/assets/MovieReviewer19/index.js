6be1a21503756a2a2bea7be49f3fe6fe79b37361
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-05-26 19:53:05 +0200

    Update

---
+++ gui (React 18) v1/src/assets/MovieReviewer19/index.js
@@ -0,0 +1,15 @@
+import mr1_19 from './preview.PNG';
+import mr2_19 from './instruction-1.PNG';
+import mr3_19 from './instruction-2.PNG';
+import mr4_19 from './instruction-3.PNG';
+import mr5_19 from './instruction-4.PNG';
+import mr6_19 from './instruction-5.PNG';
+
+export {
+  mr1_19,
+  mr2_19,
+  mr3_19,
+  mr4_19,
+  mr5_19,
+  mr6_19,
+};
