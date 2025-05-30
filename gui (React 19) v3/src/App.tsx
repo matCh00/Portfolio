8350aaba80443fbd7fcf6a616d0a58072e5e0fc4
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-05-30 23:44:11 +0200

    new project - init

--- gui (React 19) v3/src/App.tsx
+++ gui (React 19) v3/src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import {HashRouter as Router} from 'react-router-dom'
+import Hero from './sections/hero/Hero';
+import About from './sections/about/About';
+import Experience from './sections/experience/Experience';
+import Portfolio from './sections/portfolio/Portfolio';
+import Services from './sections/services/Services';
+import Contact from './sections/contact/Contact';
 
 function App() {
 
   return (
     <Router>
-      
+      <main className='app-container'>
+
+        <video autoPlay loop muted className='background-video'>
+          <source src='background.mp4' type='video/mp4' />
+        </video>
+
+        <div className="backdrop-blur"></div>
+
+        <article className="content">
+          <Hero />
+          <About />
+          <Experience />
+          <Portfolio />
+          <Services />
+          <Contact />
+        </article>
+
+      </main>
     </Router>
   );
 }
